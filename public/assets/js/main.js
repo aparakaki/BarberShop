679c6e986a11d8d0d4d387dc93cfd435d36e9fc2
Caffeineking <40303232+Caffeineking@users.noreply.github.com>
2018-10-04 15:08:51 -0700

    gif image and login done, need to fix styling

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,3 +1,26 @@
 $(".first-button").on("click" ,function (){
     alert("hello world!");
 })
+
+
+
+// jQuery & Velocity.js sign in JS
+
+function slideUpIn() {
+    $("#login").velocity("transition.slideUpIn", 1250)
+  };
+  
+  function slideLeftIn() {
+    $(".row").delay(500).velocity("transition.slideLeftIn", {stagger: 500})    
+  }
+  
+  function shake() {
+    $(".password-row").velocity("callout.shake");
+  }
+  
+  slideUpIn();
+  slideLeftIn();
+  $("button").on("click", function () {
+    shake();
+  });
+  //
