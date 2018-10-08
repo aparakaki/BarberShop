cc25311acd1323b93dfa7318fd990102b941ea28
lookrumad <verniedelacruz@gmail.com>
2018-10-07 22:31:20 -0700

    tablet userinterface created

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,51 +6,82 @@ module.exports = function (app) {
     app.get("/", function (req, res) {
         if (req.session.user) {
             console.log("1");
-            var x = req.headers.cookie;
-            var y = x.slice(((x.indexOf("="))+1), (x.indexOf(";")));
-            console.log(y);
+            console.log(req.session.user);
             res.redirect("/services");
         }
-        // else if (req.headers.cookie) {
-        //     console.log("2");
-        //     console.log(req.headers.cookie);
-        //     db.User.findOne({
-        //         where: {
-        //             token: req.headers.cookie
-        //         }
-        //     }).then(function (token) {
-        //         if (token !== null) {
-        //             req.session.user = user[i];
-        //             res.redirect("/services");
-        //         }
-        //     });
-        // }
+        else if (req.headers.cookie) {
+            console.log("2");
+            if (req.headers.cookie.match(/(?<=token=)[^ ;]*/) !== null) {
+                var cookie = req.headers.cookie.match(/(?<=token=)[^ ;]*/)[0];
+                db.User.findOne({
+                    where: {
+                        token: cookie
+                    }
+                }).then(function (token) {
+                    if (token !== null) {
+                        req.session.user = {
+                            username: req.body.username,
+                            password: req.body.password
+                        }
+                        return res.redirect("/services");
+                    }
+                    res.clearCookie("token");
+                    res.redirect("/");
+                });
+            }
+            else {
+                res.sendFile(path.join(__dirname, "../index.html"));
+            }
+        }
         else {
-            var x = req.headers.cookie;
-            var y = x.slice(((x.indexOf("="))+1), (x.indexOf(";")));
-            console.log(y);
-            console.log('3');
+            console.log("3");
             res.sendFile(path.join(__dirname, "../index.html"));
         }
     });
 
     app.get("/services", function (req, res) {
-        // if (req.session.user) {
-            res.sendFile(path.join(__dirname, "../public/services.html"));
-        // }
-        // else {
-            // res.redirect("/");
-        // }
+        if (req.session.user) {
+            db.User.findOne({
+                where: {
+                    username: req.session.user.username,
+                    password: req.session.user.password
+                }
+            }).then(function (user) {
+                if (user !== null) {
+                    console.log("services");
+                    res.sendFile(path.join(__dirname, "../public/services.html"));
+                }
+                else {
+                    res.clearCookie("token");
+                    req.session.destroy();
+                    res.redirect("/");
+                }
+            });
+        }
+        else {
+            console.log("service2");
+            res.clearCookie("token");
+            req.session.destroy();
+            res.redirect("/");
+        }
     });
 
     app.get("/calendar", function (req, res) {
-        // if (req.session.user) {
-            res.sendFile(path.join(__dirname, "../public/calendar.html"))
-        // }
-        // else {
-            // res.redirect("/");
-        // }
+        db.User.findOne({
+            where: {
+                username: req.session.user.username,
+                password: req.session.user.password
+            }
+        }).then(function (user) {
+            if (user !== null) {
+                res.sendFile(path.join(__dirname, "../public/calendar.html"));
+            }
+        });
     });
+
+    app.get("/tablet", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/tablet.html"));
+    })
 
     app.get("/logout", function (req, res) {
         res.clearCookie("token");
@@ -67,6 +98,7 @@ module.exports = function (app) {
             }
         }).then(function (token) {
             if (token !== null) {
+                console.log("here", req.body);
                 var token = "t" + Math.random();
                 db.User.update({
                     token: token,
@@ -77,17 +109,20 @@ module.exports = function (app) {
                         }
                     }).then(function (dbTodo) { });
 
-                    res.cookie("token", token, {expires: new Date(Date.now() + 90000000)});
+                res.cookie("token", token, { expires: new Date(Date.now() + 999999999) });
 
                 req.session.user = {
-                    id: req.body.id,
-                    name: req.body.name,
+                    username: req.body.username,
                     password: req.body.password
                 }
                 return res.redirect("/");
             }
-            res.send("account not found");
+            res.redirect("/userNotFound");
         });
+    });
+
+    app.get("/userNotFound", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/userNotFound.html"))
     });
 
     app.post("/createLogin", function (req, res) {
@@ -96,8 +131,44 @@ module.exports = function (app) {
             username: req.body.username,
             password: req.body.password
         }).then(function (user) {
+            req.session.user = {
+                username: req.body.username,
+                password: req.body.password
+            }
             res.redirect("/");
         })
     });
 
+    app.post("/haircutStartTime", function (req, res) {
+        console.log("hi");
+        db.tabletTrackerTime.create({
+            userName: req.body.userName,
+            startTime: req.body.startTime
+        }).then(function (user) {
+            res.json(user);
+        })
+    });
+
+    app.post("/haircutEndTime", function (req, res) {
+        db.tabletTrackerTime.findOne({
+            where: {
+                userName: req.body.userName
+            }
+        }).then(function (data) {
+            console.log(req.body.endTime);
+            let userStartTime = data.dataValues.startTime;
+            let userEndTime = parseInt(req.body.endTime);
+            let totalTime = userEndTime - userStartTime
+            db.tabletTrackerTime.update({
+                totalTime: totalTime
+            }, {
+                    where: {
+                        userName: req.body.userName
+                    }
+                }).then(function (user) {
+                    res.json(user);
+
+                })
+        });
+    })
 }
